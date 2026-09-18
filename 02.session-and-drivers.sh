b2516afa1263d925f85a1e040b132e1b18901e8b
moonbrooke <smoeland@gmail.com>
2026-09-18 15:47:29 +0700

    u

--- 02.session-and-drivers.sh
+++ 02.session-and-drivers.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 echo "=== Updating repositories and installing packages ==="
 sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
-sudo xbps-install -Sy dbus seatd polkit elogind nvidia nvidia-libs-32bit mesa-dri-32bit intel-video-accel mesa-dri intel-ucode
-sudo xbps-install -Sy os-prober ntfs-3g
+sudo xbps-install -Sy dbus elogind polkit nvidia nvidia-libs-32bit mesa-dri-32bit intel-video-accel mesa-dri intel-ucode
+sudo xbps-install -Sy pipewire wireplumber os-prober ntfs-3g
 
 echo "=== Enabling Runit Services ==="
-for service in dbus seatd polkitd; do
+for service in dbus elogind polkitd; do
     if [ ! -L "/var/service/$service" ]; then
         sudo ln -s "/etc/sv/$service" /var/service/
         echo "Enabled service: $service"
@@ -16,9 +16,6 @@ for service in dbus seatd polkitd; do
         echo "Service $service is already enabled."
     fi
 done
-
-echo "=== Adding $USER to the _seatd group ==="
-sudo usermod -aG _seatd "$USER"
 
 echo ""
 echo "Setup complete! Please reboot your system."
