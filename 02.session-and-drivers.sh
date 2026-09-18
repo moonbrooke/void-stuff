69a2785532eb1fda505e06a4f5136cf23b873e9e
moonbrooke <smoeland@gmail.com>
2026-09-18 15:06:05 +0700

    u

---
+++ 02.session-and-drivers.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "=== Updating repositories and installing packages ==="
+sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+sudo xbps-install -Sy dbus seatd polkit elogind nvidia nvidia-libs-32bit mesa-dri-32bit intel-video-accel mesa-dri
+
+echo "=== Enabling Runit Services ==="
+for service in dbus seatd polkitd; do
+    if [ ! -L "/var/service/$service" ]; then
+        sudo ln -s "/etc/sv/$service" /var/service/
+        echo "Enabled service: $service"
+    else
+        echo "Service $service is already enabled."
+    fi
+done
+
+echo "=== Adding $USER to the _seatd group ==="
+sudo usermod -aG _seatd "$USER"
+
+echo ""
+echo "Setup complete! Please reboot your system."
---
+++ 03.hyprland-packages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PACKAGES=(
+    # Desktop Environment
+    hyprland awww dunst grim slurp jq Waybar rofi hyprpicker imv wl-clipboard cliphist hyprpolkitagent rofimoji xdg-desktop-portal-hyprland nwg-look
+    
+    # Utilities
+    alsa-utils udiskie Thunar thunar-volman gvfs unzip stow fcitx5 fcitx5-mozc fcitx5-configtool
+    
+    # Terminal & stuff
+    foot neovim zsh fastfetch fzf bat btop
+    
+    # Fonts
+    noto-fonts-ttf noto-fonts-emoji noto-fonts-cjk-sans
+)
+
+echo "=== Syncing repositories and installing Void Linux packages ==="
+sudo xbps-install -Sy "${PACKAGES[@]}"
+
+echo ""
+echo "Installation completed successfully!"
